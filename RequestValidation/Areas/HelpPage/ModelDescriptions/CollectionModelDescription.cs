07fbd3cf8a31db8216576d3c5644211cb4a4c202
Sergey Savosin <savosin_sergey@mail.ru>
2017-05-01 07:42:04 +0300

    Validation Part

---
+++ RequestValidation/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace RequestValidation.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ RequestValidation/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RequestValidation.Models
+{
+    public class Employee
+    {
+        [Range(10000,99999)]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataMember(IsRequired = true)]
+        public int Department { get; set; }
+    }
+}
