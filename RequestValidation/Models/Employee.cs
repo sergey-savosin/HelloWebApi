8221ddc99c4829712fe08d3dde16dae8f721a060
Sergey Savosin <savosin_sergey@mail.ru>
2017-05-13 09:14:15 +0300

    Custom realisation of IValidatableObject

--- RequestValidation/Models/Employee.cs
+++ RequestValidation/Models/Employee.cs
@@ -3,28 +3,38 @@ using System.Runtime.Serialization;
 using Resources;
 using System.Collections.Generic;
 using RequestValidation.Utils;
+using System;
 
 namespace RequestValidation.Models
 {
-    public class Employee
+    public class Employee : IValidatableObject
     {
-        [Range(10000,99999)]
+        private const decimal PERCENTAGE = 0.75M;
+
         public int Id { get; set; }
 
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(20, ErrorMessageResourceName = "InvalidLastNameLength",
-            ErrorMessageResourceType = typeof(Messages))]
         public string LastName { get; set; }
-
-        [Required]
-        [MemberRange(0,9)]
-        public List<int> Department { get; set; }
 
         public decimal AnnualIncome { get; set; }
 
-        [LimitChecker("AnnualIncome", 75)]
         public decimal Contribution401K { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Id < 10000 || this.Id > 99999)
+                yield return new ValidationResult("Id must be in the range 10000-99999");
+
+            if (String.IsNullOrEmpty(this.LastName))
+                yield return new ValidationResult("Last name is mandatory");
+            else if (this.LastName.Length > 20)
+                yield return new ValidationResult("LastName can be only 20 characters");
+
+            if (this.Contribution401K > Decimal.Zero
+                && this.Contribution401K > this.AnnualIncome * PERCENTAGE)
+                yield return new ValidationResult(
+                    "You can contribute a maximum 75% of your annual income to 401K");
+        }
     }
 }
